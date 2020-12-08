ee4e35761757b3b3f8684af5c68225c422b448e3
shinara03 <shinara03@gmail.com>
2020-12-08 02:44:37 -0800

    Define Board#make_move and Board#next_turn

--- w4d2/MancalaHw/lib/board.rb
+++ w4d2/MancalaHw/lib/board.rb
@@ -1,5 +1,8 @@
+require "byebug" 
+
 class Board
   attr_accessor :cups
+
 
   def initialize(name1, name2)
     @name1 = name1
@@ -11,12 +14,10 @@ class Board
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
      14.times do |i|
-      if i < 6
-        @cups << [:stone] * 4
-      elsif i == 6 || i == 13
-        @cups << []
+      if i == 6 || i == 13
+         @cups << []
       else
-        @cups << [:stone] * i
+        @cups << [:stone] * 4
       end
     end
   end
@@ -27,10 +28,35 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+
+    curr_pos = start_pos
+    until stones.length == 0
+      # debugger
+      curr_pos = curr_pos + 1
+      curr_pos = 0 if curr_pos > 13
+
+      if current_player_name == @name1
+        @cups[curr_pos] << stones.pop if curr_pos != 13
+      else
+        @cups[curr_pos] << stones.pop if curr_pos != 6
+      end
+    end
+
+    render
+    next_turn(curr_pos)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      return :prompt
+    elsif @cups[ending_cup_idx].length == 1
+      return :switch
+    else
+      return ending_cup_idx
+    end
   end
 
   def render
