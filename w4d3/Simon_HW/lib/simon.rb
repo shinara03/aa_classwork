67c04a88621942da0e4dff302f3666e47037d099
shinara03 <shinara03@gmail.com>
2020-12-09 05:07:00 -0800

    modify Simon#initialize @seq and add Simon#add_random_color

--- w4d3/Simon_HW/lib/simon.rb
+++ w4d3/Simon_HW/lib/simon.rb
@@ -6,7 +6,7 @@ class Simon
   def initialize
     @sequence_length = 1 
     @game_over = false
-    @sequence = []
+    @seq = []
   end
 
   def play
@@ -26,7 +26,7 @@ class Simon
   end
 
   def add_random_color
-
+    @seq << COLORS.sample
   end
 
   def round_success_message
