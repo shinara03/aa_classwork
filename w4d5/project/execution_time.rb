63b8cd6e0dbdae2d1699629c29890f6b492ef650
shinara03 <shinara03@gmail.com>
2020-12-11 14:58:37 -0800

    Add subsum2

--- w4d5/project/execution_time.rb
+++ w4d5/project/execution_time.rb
@@ -1,4 +1,6 @@
+require "byebug"
 
+#linear time, constant space
 def my_min(list)
   min = list.first
   list.each_with_index do |ei, i|
@@ -7,18 +9,19 @@ def my_min(list)
   min
 end
 
-def compare(arr)
-  result = {}
-  arr.each_with_index do |ei, i|
-    arr.each_with_index do |ej, j|
-      if j > i 
-        result[[ei, ej]] = ei <=> ej
-      end
-    end
-  end
-  result
-end
+# def compare(arr)
+#   result = {}
+#   arr.each_with_index do |ei, i|
+#     arr.each_with_index do |ej, j|
+#       if j > i 
+#         result[[ei, ej]] = ei <=> ej
+#       end
+#     end
+#   end
+#   result
+# end
 
+#n^2, constant space
 def my_min_2(list)
 
   min = 1.0/0.0
@@ -36,6 +39,7 @@ end
 # p my_min_2(list)  # =>  -5
 
 #largest_contiguous_subsum
+# n^3, n^2 - n => n^2
 def subsum(list)
   
   sublist = []
@@ -60,17 +64,65 @@ end
 # list = [2, 3, -6, 7, -6, 7]
 # p subsum(list)
 # list = [-5, -1, -3]
-# p subsum(list)
+# p _subsum(list)
 
 
 def _subsum(list)
   largest_sum = -1.0/0.0
   current_sum = 0
-   finish=1
-  list.each do |ei, i|
-    _sub = list[i..finish].sum
-    current_sum+=
-  end 
+  
+
+  i = 0 
+  start, finish = 0 , 0
+  until start == list.length 
+
+    # debugger
+
+    start = i
+    if finish >= list.length
+      finish = start 
+      i += 1
+    end 
+    current_sum = list[start..finish].sum
+    largest_sum = current_sum if largest_sum < current_sum && !list[start..finish].empty?
+
+    finish += 1
+  end
+
+
+ # [1, 2, -4, 4, -3, 4]
+ # largest = [1,3,3,4,4,]
+ # current = [1,3,-1,4,1,5]
+
+ # 
 
   largest_sum
 end
+
+def subsum2(list)
+  largest = list.first
+  current = list.first
+
+  (1..list.length-1).each do |i|
+    if current < 0
+      current = list[i]
+    else
+      current += list[i]
+    end
+    
+    if current > largest 
+      largest = current 
+    end
+  end
+
+  largest
+end
+
+list = [5, 3, -7]
+# [5]
+# [5]
+p subsum2(list)
+list = [2, 3, -6, 7, -6, 7]
+p subsum2(list)
+list = [-5, -1, -3]
+p subsum2(list)
