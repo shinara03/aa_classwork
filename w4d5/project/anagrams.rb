5c82b3c97c2f1a81a53e45d18c7633ecdceb6188
Michael Clayton Noble <makos.code@gmail.com>
2020-12-11 16:13:22 -0800

    implement a 4th version of anagrams

--- w4d5/project/anagrams.rb
+++ w4d5/project/anagrams.rb
@@ -31,14 +31,23 @@ def second_anagram?(string_a, string_b)
   string_b_arr.empty? && string_a.length == string_b.length
 end
 
-# p second_anagram?("cat", "tac")
+
 
 # n^2 or nlog(n) 
 def third_anagram?(string_a, string_b)
 
   string_a.split("").sort == string_b.split("").sort
-
-
 end
 
-p third_anagram?("tacc", "tac")
+
+def fourth_anagram?(string_a, string_b)
+
+  hash_1 = Hash.new(0)
+  hash_2 = Hash.new(0)
+
+  string_a.each_char{|c| hash_1[c]+=1 }
+  string_b.each_char{|c| hash_2[c]+=1 }
+
+  hash_1 == hash_2
+end
+p fourth_anagram?("cat", "tac")
