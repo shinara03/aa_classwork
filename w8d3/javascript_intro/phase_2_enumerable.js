08d3ffe19423d9610a2b24afc8956e67d6392242
shinara03 <shinara03@gmail.com>
2021-01-13 14:39:42 -0800

    finish myMap

--- w8d3/javascript_intro/phase_2_enumerable.js
+++ w8d3/javascript_intro/phase_2_enumerable.js
@@ -10,3 +10,17 @@ function doubler(n) {
 
 // console.log([1, 2, 3, 4, 5].myEach(doubler));
 
+Array.prototype.myMap = function(cb) {
+    let mapped = [];
+    this.myEach((el) => {
+        mapped.push(cb(el));
+    })
+
+    return mapped;
+};
+
+function doubler(n) {
+    return 2 * n;
+}
+
+console.log([1, 2, 3, 4, 5].myMap(doubler))
