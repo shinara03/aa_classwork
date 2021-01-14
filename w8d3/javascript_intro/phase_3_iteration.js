257926863f1eccaa874971038f3a49b911a7cedd
shinara03 <shinara03@gmail.com>
2021-01-13 16:55:58 -0800

    finish phase 3

--- w8d3/javascript_intro/phase_3_iteration.js
+++ w8d3/javascript_intro/phase_3_iteration.js
@@ -19,4 +19,22 @@ Array.prototype.bubble_sort = function() {
 }
 
 // console.log([1, 5, 3, 2, 6, 10, 0].bubble_sort())
+String.prototype.substrings = function() {
+    let subs = [];
+    chars = this.split("");
+    // h e l l o
+    for(let i=0; i < chars.length; i++) {
+        // if (i === chars.length - 1) {
+        //     subs.push(chars.slice(i));
+        // }
+        for(let j = i; j < chars.length; j++) {
+            // if (chars.indexOf(chars.slice(i, j + 1).join(""))=== -1) {
+            // }
+            subs.push(chars.slice(i, j+1).join(""))
+        }
+    }
+    return subs;
+};
+
+console.log("hello".substrings())
 
