55cacc5aa70ca7416235e211354ee9735c9c805c
bcdguz <bcdguz96@gmail.com>
2021-01-19 15:40:50 -0800

    Creating flappy_bird

---
+++ w9d1/flappy_bird/src/game.js
@@ -0,0 +1,14 @@
+import Level from './level';
+
+export default class FlappyBird {
+  constructor(canvas){
+    this.ctx = canvas.getContext("2d");
+    this.dimensions = { width: canvas.width, height: canvas.height };
+  }
+
+  restart //to be written\
+  //contains @level
+
+  animate //
+  //calls @level.animate();
+}
