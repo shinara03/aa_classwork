e988b8c3c2da678287d6950656ba9da09dd2690a
shinara03 <shinara03@gmail.com>
2021-02-02 12:19:44 -0800

    created show and index jbuilder file

---
+++ w11d2/jbuilder_project/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Your routes here!
+  namespace :api, defaults: {format: :json} do 
+    resources :guests, only: [:show, :index]
+  end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
